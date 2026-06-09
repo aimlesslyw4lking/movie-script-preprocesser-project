33d9b3286a05b23aa10c7405a4afe95f5b015a44
AimlesslyWalking <dddavtian@edu.hse.ru>
2026-06-09 22:25:13 +0300

    Implement processor.py

--- processor.py
+++ processor.py
@@ -1,1 +1,157 @@
-pass
+from pathlib import Path
+import pymorphy3
+from spacy.lang.ru import Russian
+
+
+
+class ScriptNotFoundError(Exception):
+    def __init__(self, message="No scripts found! :( n\
+              Try to manually check your raw_scripts folder"):
+        super().__init__(message)
+
+class ScriptCompilationError(Exception):
+    def __init__(self, message="Oh no! Something went wrong during the compilation..."):
+        super().__init__(message)
+
+class ScriptPreprocessingError(Exception):
+    def __init__(self, message="Oh no! Something went wrong during the final stage of preprocessing..."):
+        super().__init__(message)
+
+class CleanScriptNotWrittenError(Exception):
+    def __init__(self, message="It looks like the clean script has failed to be written..."):
+        super().__init__(message)
+
+
+def find_scripts():
+    """
+    Finds the path containing .txt script files.
+    """
+    pathlist = Path("./raw_scripts").rglob('*.txt')
+    return (path for path in pathlist)
+
+    
+def tokenize_and_compile(find_scripts) -> dict[str, list[str]]:
+    """
+    Tokenizes a .txt file in the script folder according to the format.
+    Compiles each character found in the script and their
+    respective lines into key-value pairs.
+
+    Args: ---
+
+    Returns: dict[str, list[str]]
+    """
+    char_lines = {}
+    
+    for path in find_scripts:
+        for line in path.read_text(encoding='utf-8').splitlines():
+            line = line.strip()
+            
+            if line.startswith('[') and ']' in line:
+                parts = line.split("]", 1)
+                
+                name = parts[0].replace('[', '').strip().lower().replace('ё', 'е')
+                speech = parts[1]
+                
+                clean_tokens = []
+                for word in speech.lower().split():
+                    cleaned = ''.join(char for char in word if char.isalnum())
+                    if cleaned:
+                        clean_tokens.append(cleaned)
+                
+                if name not in char_lines:
+                    char_lines[name] = []
+                
+                char_lines[name].extend(clean_tokens)
+                
+    return char_lines
+
+def lemmatize_and_clean(char_lines: dict) -> dict[str, int]:
+    """
+    Lemmatizes and cleans the compiled dictionary of character lines.
+    
+    Args: char_lines: dict[str, int] - Key-value pairs of the character
+    and their tokenized lines
+
+    Returns: char_lines_clean: dict[str, int] - Key-value pairs of the character
+    and their lines that are tokenized and cleaned from most popular stopwords
+    """
+    
+    morph = pymorphy3.MorphAnalyzer()
+    stopwords = Russian().Defaults.stop_words
+
+    char_lines_clean = {}
+
+    for name in char_lines:
+        char_lines[name] = [
+            morph.parse(token)[0].normal_form
+            for token in char_lines[name]
+        ]
+
+        char_lines_clean[name] = [
+            word for word in char_lines[name]
+            if word not in stopwords
+            ]
+
+    return char_lines_clean
+
+def write_cleaned_file(char_lines_clean: dict[str, int]) -> bool:
+    """ 
+    Creates a corpus-ready .txt file. 
+    
+    Args: char_lines_clean: dict[str, int] 
+
+    Returns: a corpus-ready file for you to enjoy!!! (or suffer bc of projects!)
+    
+    """
+    try:
+        output_dir = Path("./cleaned_scripts")
+        
+        output_dir.mkdir(parents=True, exist_ok=True)
+        
+        for name, tokens in char_lines_clean.items():
+            no_space_name = name.replace(" ", "_")
+            file_path = output_dir / f'{no_space_name}_corpus.txt'
+            
+            file_words = " ".join(tokens)
+        
+            file_path.write_text(file_words, encoding='utf-8')
+            print(f"File saved! : {file_path}")
+            
+        return True
+        
+    except CleanScriptNotWrittenError as e:
+        print(f"Oh no! Something went wrong during the writing process: {e}")
+        return False
+
+def main():
+
+    """ 
+    Launches implementation (and treats you like a cutie pie)
+    """
+
+    print('Hi!! <3      Looking for your scripts now...')
+    scripts = find_scripts()
+    
+    if not scripts:
+        raise ScriptNotFoundError()
+        
+    print("Scripts found! Let's compile them now <3")
+    compiled_scripts = tokenize_and_compile(scripts)
+    
+    if not compiled_scripts:
+        raise ScriptCompilationError()
+        
+    print("Your scripts have been compiled successfully! ^_^ ")
+    cleaned_scripts = lemmatize_and_clean(compiled_scripts)
+
+    if not cleaned_scripts:
+        raise ScriptPreprocessingError()
+        
+    print("Yay! The scripts are ready! Let's upload them to a folder now")
+    uploaded_scripts = write_cleaned_file(cleaned_scripts)
+    
+    if not uploaded_scripts:
+        raise CleanScriptNotWrittenError()
+
+if __name__ == "__main__":
+    main()
