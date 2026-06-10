62ea9097d4c4b780c98f955064b950893d232b19
AimlesslyWalking <dddavtian@edu.hse.ru>
2026-06-10 04:05:52 +0300

    Separate main() into a file that launches the implementation

---
+++ main.py
@@ -0,0 +1,36 @@
+"""Launch implementation"""
+
+import processor as pr
+
+def main():
+
+    """ 
+    Launches implementation (and treats you like a cutie pie)
+    """
+
+    print('Hi!! <3    Looking for your scripts now...')
+    scripts = pr.find_scripts()
+    
+    if not scripts:
+        raise pr.ScriptNotFoundError("Uh oh... There is no script in the destinated folder...")
+        
+    print("Scripts found! Let's compile them now <3")
+    compiled_scripts = pr.tokenize_and_compile(scripts)
+    
+    if not compiled_scripts:
+        raise pr.ScriptCompilationError("Oops! Something went wrong during the compilation of your script!")
+        
+    print("Your scripts have been compiled successfully! ^_^ ")
+    cleaned_scripts = pr.lemmatize_and_clean(compiled_scripts)
+
+    if not cleaned_scripts:
+        raise pr.ScriptPreprocessingError("I'm sorry! Something happened as the scripts were preprocessed..")
+        
+    print("Yay! The scripts are ready! Let's upload them to a folder now")
+    uploaded_scripts = pr.write_cleaned_file(cleaned_scripts)
+    
+    if not uploaded_scripts:
+        raise pr.CleanScriptNotWrittenError("")
+
+if __name__ == "__main__":
+    main()
